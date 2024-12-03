aa9b9ce44db21273aada02b6cf3dc751dc656d2d
JarysonUERN <jaryson20230019388@alu.uern.br>
2024-12-03 14:02:55 -0300

    Update ClienteBancario.java

--- src/Clientes/ClienteBancario.java
+++ src/Clientes/ClienteBancario.java
@@ -34,3 +34,70 @@ public class ClienteBancario {
                 '}';
     }
 }
+
+
+        public class LCartao {
+             public Cartao(String numero, String nomeTitular, String validade, String codigoSeguranca, double saldo) {
+        this.numero = numero;
+        this.nomeTitular = nomeTitular;
+        this.validade = validade;
+        this.codigoSeguranca = codigoSeguranca;
+        this.saldo = saldo;
+    }
+
+    // Getters e Setters
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public String getCodigoSeguranca() {
+        return codigoSeguranca;
+    }
+
+    public void setCodigoSeguranca(String codigoSeguranca) {
+        this.codigoSeguranca = codigoSeguranca;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Cartao{" +
+                "numero='" + numero + '\'' +
+                ", nomeTitular='" + nomeTitular + '\'' +
+                ", validade='" + validade + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+
+
+
+
+}
